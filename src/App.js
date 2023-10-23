003999b55ce60fbe91458afc6ae92f3d8b6c8fba
Navaneeth-R-Krishnan <rnavaneeth19@gmail.com>
2023-10-23 16:29:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import questions from './questions';
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerClick = (selectedAnswer) => {
+    if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const handleRetakeClick = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  };
+
+  useEffect(() => {
+    if (showScore) {
+      // You can perform any final actions here when the quiz ends.
+    }
+  }, [showScore]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div>
+          <div className="score-section">
+            You scored {score} out of {questions.length}
+          </div>
+          <button onClick={handleRetakeClick} className="retake-button">
+            Retake
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              Question {currentQuestion + 1} of {questions.length}
+            </div>
+            <div className="question-text">{questions[currentQuestion].question}</div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].options.map((option, index) => (
+              <button key={index} onClick={() => handleAnswerClick(option)}>
+                {option}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+      question: 'What does HTML stand for?',
+      options: ['Hypertext Markup Language', 'Highly Textured Modern Language', 'Hyperlink and Text Markup Language', 'Home Tool Markup Language'],
+      correctAnswer: 'Hypertext Markup Language',
+    },
+    {
+      question: 'Which of the following is a CSS preprocessor?',
+      options: ['HTML', 'React', 'Sass', 'JavaScript'],
+      correctAnswer: 'Sass',
+    },
+    {
+      question: 'What is the primary purpose of the <div> element in HTML?',
+      options: ['Define a division or a section', 'Display an image', 'Create a hyperlink', 'Add a form field'],
+      correctAnswer: 'Define a division or a section',
+    },
+    {
+      question: 'Which CSS property is used to change the text color of an element?',
+      options: ['text-color', 'font-color', 'color', 'text-style'],
+      correctAnswer: 'color',
+    },
+    {
+      question: 'In React, what is used to pass data from a parent component to a child component?',
+      options: ['Props', 'State', 'Context', 'Redux'],
+      correctAnswer: 'Props',
+    },
+  ];
+  
+  export default questions;
+  
